d89970241df056f98b78f93a03b9ea2a961976ce
josh-lfs <josh-lfs@gmail.com>
2023-12-29 11:34:24 -0800

    add dynamic data

---
+++ src/_data/news.js
@@ -0,0 +1,13 @@
+// import axios from 'axios';
+const axios = require('axios');
+require('dotenv').config();
+console.log(process.env)
+
+module.exports = async function() {
+  try {
+    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.API_KEY}&pageSize=5`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
